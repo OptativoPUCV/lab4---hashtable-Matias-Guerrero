11255697f8ab983dbe48791ea40ea783e3023ac5
Your Name <you@example.com>
2023-04-12 16:37:00 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -43,7 +43,7 @@ void insertMap(HashMap * map, char * key, void * value)
 {
     int posicion = hash(key, map->capacity);
 
-    while(map->buckets[posicion] != NULL)
+    while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL && is_equal(map->buckets[posicion]->key, key) != 1)
     {
         posicion = (posicion + 1) % map->capacity;
     }
